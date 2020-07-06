f85b575897e03bd217d36e78e18596061d887b28
jeongjoonwoo <jw.jeong@zipida.com>
2020-07-06 20:01:48 +0900

    Fix javascript code
    -변수명은 camelCase로 작성 변경하였습니다
    -var을 모두 let과 const로 변경하였습니다.
    -window.event.keycode를 event.key로 변경하였습니다.
    -localStorage를 하나씩 저장하지 않고 배열로 입력하여 저장하였습니다.
    -삭제,온로드시 다시보여주는 화면까지 완료하였습니다.

--- jw.jeong/work1-1/app.js
+++ jw.jeong/work1-1/app.js
@@ -1,40 +1,34 @@
 const root = document.getElementById('root');
 
-/*
-root.children
-0: h1
-1: div.add-section
-2: h3
-3: ul#incomplete-tasks
-*/
-/*
-<div class="add-section">.children
-0: label
-1: input#new-task
-2: button#add-button
- */
-var toDo_text = root.children[1].children[1],
-    toDo_button = root.children[1].children[2],
-    toDo_list = root.children[3];
+/* 수정전 var todoText = root.children[1].children[1],
+todoButton = root.children[1].children[2],
+todoList = root.children[3];*/
+const todoText = root.querySelector("#new-task"), // o
+    todoButton = root.querySelector("#add-button"),
+    todoTask = root.querySelector("#incomplete-tasks");
 
-var storage = localStorage;
-
+const storage = localStorage;
 function getTodo(e){  
-    var keycode = window.event.keyCode
-    var toDo = toDo_text.value;
-    if((keycode == 13 || e.type=='click') && toDo !== ''){ // enter의 keyCode는 13 || e.type == click 둘 중하나의 이벤트가 실행이되면 입력 이벤트가 실행합니다.
-        var storageKey = toDo; //key값에 스토리지 길이만큼 입력
-        storage.setItem(storageKey,toDo); // localstroage 저장
-        toDo_list.append(createIndex(toDo)); // tasks 버튼추가 
-        toDo_text.value = '';
+    const keyCode = e.key;
+    const todo = todoText.value;
+    if(storage.key('todoList')==null){
+        storage.setItem('todoList',JSON.stringify([]));
+    }
+    if((keyCode == 'Enter' || e.type=='click') && todo !== ''){ // enter의 keyCode는 13 || e.type == click 둘 중하나의 이벤트가 실행이되면 입력 이벤트가 실행합니다.
+        let todoList = localStorage.getItem('todoList');
+        todoList = JSON.parse(todoList);
+        todoList.push(todo);
+        storage.setItem('todoList',JSON.stringify(todoList));
+        todoTask.append(createIndex(todo)); // tasks 버튼추가 
+        todoText.value = '';
     }
 }
 
-function createIndex(toDo){
-    var div = document.createElement('ul'),
+function createIndex(todo){
+    let div = document.createElement('ul'),
         button = document.createElement('input');
-    div.className=toDo; 
-    div.innerHTML=toDo;
+    div.className=todo; 
+    div.innerHTML=todo;
     button.type = 'button';
     button.value = 'Delete';
     button.addEventListener('click',deleteTodo); //삭제 이벤트
@@ -43,25 +37,35 @@ function createIndex(toDo){
 }
 
 function deleteTodo(e){
-    var deleteIndex = e.target.parentNode; //e.target은 button을 추적하여 부모노드를 추적합니다.
-    storage.removeItem(deleteIndex.className);//localStorage에서 제거해줍니다.
-    toDo_list.removeChild(deleteIndex);//todo list에서 항목 삭제합니다.
-    
+    let todoList = JSON.parse(localStorage.getItem('todoList'));
+    let deleteValue = e.target.parentNode; //e.target은 button을 추적하여 부모노드를 추적합니다.
+    let deleteList=[]
+    //filter를 사용하면 스토리지에서 받아온 값을 각각 task에 담아서 객체 하나씩 사용할 수 있습니다
+    //task랑 삭제하려는 값과 동일한지 안한지 체크를 한 후 이 값과 동일하지 않은값을 delete리스트에 넣어줍니다.
+    //delete을 다시 todoList를 이름으로 스토리지에 setItem해줍니다.
+    todoList = todoList.filter(task => { 
+        if(task !=deleteValue.className){
+            deleteList.push(task);
+        }
+    })
+    storage.setItem('todoList',JSON.stringify(deleteList));
+    todoTask.removeChild(deleteValue);//todo list에서 항목 삭제합니다.   
 }
 
-function getStorage(){//화면의 다시 온로드하면 실행합니다.
-    let stoargeLength = storage.length;
-    for(let i=0;i<=stoargeLength-1;i++){
-        let storageKey = storage.key(i) //key값을 길이만큼 받아옵니다.
-        toDo_list.append(createIndex(storage.getItem(storageKey))) //todo list에 차례대로 쌓아줍니다.
-    }
+function getStorage(){
+    //삭제하는 방법과 동일하게 task에 담아서 하나씩 생성해주었습니다.
+    let todoList = JSON.parse(localStorage.getItem('todoList'));
+    todoList.filter(task =>{
+        todoTask.append(createIndex(task)); // tasks 버튼추가 
+    })
 }
+
 
 function init(){
-    toDo_text.addEventListener("keydown",getTodo);
+    todoText.addEventListener("keydown",getTodo);
     window.onload = function(){
         getStorage()
     }
-    toDo_button.addEventListener("click",getTodo);
+    todoButton.addEventListener("click",getTodo);
 }
 init()
