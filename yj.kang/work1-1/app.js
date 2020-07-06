0c73f5c16f4948688707203fcf1cd10735d37744
YongJun_Kang <yj.kang@zipida.com>
2020-07-07 02:18:21 +0900

    FIX APP.JS
    - e.key 이벤트로 변경 후 한글이 두번 입력되는 버그를 고쳤습니다.
    - 선택된 목록 제거 후 localstorage에  아이디를 초기화 시켜 저장합니다.
    (수정) html의 li id="" 를 초기화 시켜줘야 합니다.

--- yj.kang/work1-1/app.js
+++ yj.kang/work1-1/app.js
@@ -6,12 +6,10 @@ const todoForm = root.querySelector('#new-task') // // HTML <input id="new-task"
 
 let toDos = []; // todolist를 담기위한 localstorage key "toDos" 배열
 
-function addTodo(event) { // 버튼 클릭 이벤트
+function addTodo(e) { // 버튼 클릭 이벤트
     const todoValue = todoForm.value;
-    const keyCode = event.keyCode;
-    const clickBtn = event.type;
 
-    if (keyCode=="13" || clickBtn=="click") { // 엔터키를 누르거나 버튼 클릭시 이벤트 발생
+    if (e.key==='Enter' || e.type=="click") { // 엔터키를 누르거나 버튼 클릭시 이벤트 발생
         if(todoValue=="") { // 입력 받은 후 빈칸 체크
             alert("빈칸입니다.") // 에러 메시지
             todoForm.focus(); // 입력창 포커스
@@ -25,9 +23,9 @@ function addTodo(event) { // 버튼 클릭 이벤트
 function insertTodo(text) {
     let li = document.createElement("li"); // li 동적 생성
     let deleteBtn = document.createElement("button"); // Delete Button 동적 생성
-    let todoId = toDos.length + 1 // 배열 길이에 맞게 id 생성
+    let newId = toDos.length + 1;
     li.innerText = text; // todoValue 삽입
-    li.id = todoId; // 삭제를 위해 localStorage ID와 동일하게 부여
+    li.id = newId; // 삭제를 위해 localStorage ID와 동일하게 부여
     deleteBtn.innerText = "Delete"; // Delete Text 삽입
     deleteBtn.id = "deleteBtn"
     deleteBtn.addEventListener("click", deleteTodo);
@@ -35,7 +33,7 @@ function insertTodo(text) {
     todoList.appendChild(li);
 
     todoObj = {
-        id : todoId, // 비교 삭제를 위한 아이디 부여
+        id : newId, // 비교 삭제를 위한 아이디 부여
         text : text // todoText 저장
     };
 
@@ -49,13 +47,25 @@ function deleteTodo(event) {
     let liId = liDelete.id;
     liDelete.remove();
 
-    const cleanTodo = toDos.filter(function(e) {
-        return e.id !== parseInt(liId);
+    let resetTodo = []; // filter로 해당 todolist 삭제 후 id값 초기화를 위한 배열
+
+    const cleanTodo = toDos.filter(function(e) { 
+        return e.id !== parseInt(liId); // toDos에서 선택된 id를 제외한 나머지를 찾아온다.
     })
 
-    toDos = cleanTodo; // filter된 cleanTodo 객체를 toDos에 교체
-    saveTodo(); // localstorage에 바뀐 toDos를 삽입
+    for(let i = 1; i <= cleanTodo.length; i++) { // 선택된 todolist 제거후 id값 초기화
+        let resetObj = {}; // Obj
+        resetObj['id'] = i
+        resetObj['text'] = cleanTodo[i-1].text;
+        liDelete.id = i;
+        resetTodo.push(resetObj); // 해당 Obj를 resetTodo에 push
+    }
+    console.log(resetTodo);
+
+    toDos = resetTodo; // toDos를 최종정리된 resetTodo로 변경
+    saveTodo(toDos); // 저장
 }
+    
 
 function saveTodo() {
     localStorage.setItem("toDos", JSON.stringify(toDos)); // obj -> string
@@ -74,7 +84,7 @@ function loadTodo() {
 
 
 function init() {
-    todoForm.addEventListener("keydown", addTodo);
+    todoForm.addEventListener("keypress", addTodo); // 한글 2번 입력 오류로 keydown -> keypress 로 변경
     addButton.addEventListener("click", addTodo);
     loadTodo();
 }
