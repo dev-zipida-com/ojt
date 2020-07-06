b27d0bc567103bc12a38a491bbd2707cdf8e0b74
YongJun_Kang <yj.kang@zipida.com>
2020-07-06 19:30:01 +0900

    FIX TODOLIST
    변수명을 camelCase로 변경했습니다.
    var를 용도에 따라 let, const로 변경했습니다.
    html 태그 가져올 때 querySelecter를 사용했습니다.
    이벤트 리스너 부분을 변경했습니다.

--- yj.kang/work1-1/app.js
+++ yj.kang/work1-1/app.js
@@ -1,14 +1,17 @@
 const root = document.getElementById('root'); // HTML <div id="root">
 
-const addButton = root.childNodes[3].childNodes[5]; // HTML <input id="add-button">
-const todoList = root.childNodes[7] // HTML <ul id="incomplete-tasks">
-const todoForm = root.childNodes[3].childNodes[3] // // HTML <input id="new-task">
+const addButton = root.querySelector('#add-button'); // HTML <input id="add-button">
+const todoList = root.querySelector('#incomplete-tasks') // HTML <ul id="incomplete-tasks">
+const todoForm = root.querySelector('#new-task') // // HTML <input id="new-task">
 
-var toDos = []; // todolist를 담기위한 localstorage key "toDos" 배열
+let toDos = []; // todolist를 담기위한 localstorage key "toDos" 배열
 
-function addTodo(e) { // 버튼 클릭 이벤트
-    var todoValue = todoForm.value;
-    if (window.event.keyCode==13 || e.type=="click") { // 엔터키를 누르거나 버튼 클릭시 이벤트 발생
+function addTodo(event) { // 버튼 클릭 이벤트
+    const todoValue = todoForm.value;
+    const keyCode = event.keyCode;
+    const clickBtn = event.type;
+
+    if (keyCode=="13" || clickBtn=="click") { // 엔터키를 누르거나 버튼 클릭시 이벤트 발생
         if(todoValue=="") { // 입력 받은 후 빈칸 체크
             alert("빈칸입니다.") // 에러 메시지
             todoForm.focus(); // 입력창 포커스
@@ -20,9 +23,9 @@ function addTodo(e) { // 버튼 클릭 이벤트
 }
 
 function insertTodo(text) {
-    var li = document.createElement("li"); // li 동적 생성
-    var deleteBtn = document.createElement("button"); // Delete Button 동적 생성
-    var todoId = toDos.length + 1 // 배열 길이에 맞게 id 생성
+    let li = document.createElement("li"); // li 동적 생성
+    let deleteBtn = document.createElement("button"); // Delete Button 동적 생성
+    let todoId = toDos.length + 1 // 배열 길이에 맞게 id 생성
     li.innerText = text; // todoValue 삽입
     li.id = todoId; // 삭제를 위해 localStorage ID와 동일하게 부여
     deleteBtn.innerText = "Delete"; // Delete Text 삽입
@@ -42,13 +45,12 @@ function insertTodo(text) {
 }
 
 function deleteTodo(event) {
-    var del_li = event.target.parentNode; //선택된 deletBtn의 부모노드 li
-    var id_li = del_li.id;
-    del_li.remove();
+    const liDelete = event.target.parentNode; //선택된 deletBtn의 부모노드 li
+    let liId = liDelete.id;
+    liDelete.remove();
 
-    console.log(toDos);
     const cleanTodo = toDos.filter(function(e) {
-        return e.id !== parseInt(id_li);
+        return e.id !== parseInt(liId);
     })
 
     toDos = cleanTodo; // filter된 cleanTodo 객체를 toDos에 교체
@@ -61,10 +63,10 @@ function saveTodo() {
 }
 
 function loadTodo() {
-    var local_Todo = localStorage.getItem("toDos"); // localStorage Key "toDos" 가져오기
-    var parse_Todo = JSON.parse(local_Todo); // string -> obj
-    if(parse_Todo != null) {
-        parse_Todo.forEach(function(e) { // toDos Value => text 값을 insertTodo로 전송
+    const todoLoacl = localStorage.getItem("toDos"); // localStorage Key "toDos" 가져오기
+    let todoParse = JSON.parse(todoLoacl); // string -> obj
+    if(todoParse != null) {
+        todoParse.forEach(function(e) { // toDos Value => text 값을 insertTodo로 전송
             insertTodo(e.text);
         });
     }
