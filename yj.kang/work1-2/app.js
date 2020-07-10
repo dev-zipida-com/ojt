82d509c89931a5654a47a3e5986ae30c4b7ce4ff
YongJun_Kang <yj.kang@zipida.com>
2020-07-10 10:32:00 +0900

    Fix app.js
    - 새로운 변수를 생성하여 인자 값을 할당
    - Data 분류를 위한 예외처리 방식 변경
    - Data 참조 반복 삭제
    - for문 삭제 및 array 내장 메소드로 변경

--- yj.kang/work1-2/app.js
+++ yj.kang/work1-2/app.js
@@ -6,49 +6,45 @@ const table = root.querySelector('#incomplete-table');
 const thead = root.querySelector('#table-head');
 const tbody = root.querySelector('#table-body');
 
-function createThead(obj) { // thead row 생성 함수
-    let headRow = thead.insertRow(0);
-    let objKey = Object.keys(obj);
+function createThead(data) { // thead row 생성 함수
+    const headRow = thead.insertRow(0);
+    const dataKey = Object.keys(data);
 
-    objKey.forEach((data,index)=> {
+    dataKey.forEach((data,index)=> {
         headRow.insertCell(index).innerHTML = data;
     })
 }
 
-function createTbody(obj) { // tbody row 생성 함수
-    let row = tbody.insertRow(-1); // 0을 하면 24부터 삽입 -1을 하면 1부터 삽입
+function createTbody(data) { // tbody row 생성 함수
+    const row = tbody.insertRow(-1); // 0을 하면 24부터 삽입 -1을 하면 1부터 삽입
+    const tableValue = Object.values(data);
 
-    obj = Object.values(obj);
-    obj.forEach((data,index)=> { // data 값, index 값의 번호
+    tableValue.forEach((data,index)=> { // data 값, index 값의 번호
         row.insertCell(index).innerHTML = data; // idex 번호로 cell 번호를 지정하고 data를 삽입한다.
     });
 }
 
 function newObj(data) { // fetchData의 JSON을 객체에 담는다.
-    let obj = {};
-
-    if(data !== "success") { // undefined 값 방지
-        obj = data;
-        createThead(obj[0]);
-        for(let i in obj) {
-            createTbody(obj[i]);
-        }
+    if(Array.isArray(data)) { // 인자가 Array인지 판별한 후 Array만 받아온다.
+        createThead(data[0]);
+        data.forEach((data) => {
+            createTbody(data);
+        });
     }
 }
 
 function fetchData() { // JSON 파싱
-    fetch("http://dummy.restapiexample.com/api/v1/employees")
+    //fetch("http://dummy.restapiexample.com/api/v1/employees")
+    fetch("https://jsonplaceholder.typicode.com/todos")
     .then((res) => res.json())
     .then((json) => {
-        for(let i in json) {
-            newObj(json[i])
-        }
+            newObj(json);
     })
 }
 
 
-function tableCheck(row) { // 테이블 중복생성 방지
-    let tbodyCount = tbody.childElementCount;
+function tableCheck() { // 테이블 중복생성 방지
+    const tbodyCount = tbody.childElementCount;
     if(tbodyCount === 0) { // tbody에 요소가 없으면 테이블을 그려준다.
         fetchData();
     } else { // 요소가 있으면 중복 생성을 방지하기 위해서 지우고 새로 만들어준다.
