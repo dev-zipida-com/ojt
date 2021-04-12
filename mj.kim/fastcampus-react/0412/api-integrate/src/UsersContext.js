f9f7ebc8aaac1dcc2d0d7bc1064e5614c4bd20f0
MJun-Kim <mj.kim@zipida.com>
2021-04-12 18:56:47 +0900

    Add fastcampus react-lecture, react-todoList
    - add react-lecture practice project
    - add react-todoList project

---
+++ mj.kim/fastcampus-react/0412/api-integrate/src/UsersContext.js
@@ -0,0 +1,84 @@
+import React, { createContext, useReducer, useContext } from "react";
+import * as api from "./api";
+import createAsyncDispatcher, { initialAsyncState } from "./asyncActionUtil";
+
+const initialState = {
+  users: initialAsyncState,
+  user: initialAsyncState,
+};
+
+// GET_USERS
+// GET_USERS_SUCCESS
+// GET_USERS_ERROR
+// GET_USER
+// GET_USER_SUCCESS
+// GET_USER_ERROR
+function usersReducer(state, action) {
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: loadingState,
+      };
+    case "GET_USERS_SUCCESS":
+      return {
+        ...state,
+        users: success(action.data),
+      };
+    case "GET_USERS_ERROR":
+      return {
+        ...state,
+        users: error(action.error),
+      };
+    case "GET_USER":
+      return {
+        ...state,
+        user: loadingState,
+      };
+    case "GET_USER_SUCCESS":
+      return {
+        ...state,
+        user: success(action.data),
+      };
+    case "GET_USER_ERROR":
+      return {
+        ...state,
+        user: error(action.error),
+      };
+    default:
+      throw new Error("Unhandled action type", action.type);
+  }
+}
+
+const UsersStateContext = createContext(null);
+const UsersDispatchContext = createContext(null);
+
+export function UsersProvider({ children }) {
+  const [state, dispatch] = useReducer(usersReducer, initialState);
+  return (
+    <UsersStateContext.Provider value={state}>
+      <UsersDispatchContext.Provider value={dispatch}>
+        {children}
+      </UsersDispatchContext.Provider>
+    </UsersStateContext.Provider>
+  );
+}
+
+export function useUsersState() {
+  const state = useContext(UsersStateContext);
+  if (!state) {
+    throw new Error("Cannot find UserProvider");
+  }
+  return state;
+}
+
+export function useUsersDispatch() {
+  const dispatch = useContext(UsersDispatchContext);
+  if (!dispatch) {
+    throw new Error("Cannot find UserProvider");
+  }
+  return dispatch;
+}
+
+export const getUsers = createAsyncDispatcher("GET_USERS", api.getUsers);
+export const getUser = createAsyncDispatcher("GET_USER", api.getUser);
