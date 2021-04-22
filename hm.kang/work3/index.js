6e475befdeda3255137231f9a4a484365b6f88e9
Hyeonmyeong <hm.kang@zipida.com>
2021-04-22 11:58:38 +0900

    add work3 study

--- hm.kang/work3/index.js
+++ hm.kang/work3/index.js
@@ -24,23 +24,16 @@ var clockTime = document.getElementById("clock-time");
 
 
 Date.prototype.getMyTime = function(){
-    let year = this.getFullYear();
-    let month = this.getMonth();
-    let date = this.getDate();
-
-    let hour = this.getHours();
-    let minutes  = this.getMinutes();
-    let seconds = this.getSeconds();
-
+    let year = leadingZeros( this.getFullYear(),2 );
+    let month = leadingZeros(this.getMonth(),2);
+    let date = leadingZeros(this.getDate(),2);
+    let hour = leadingZeros(this.getHours(),2);
+    let minutes  = leadingZeros(this.getMinutes(),2);
+    let seconds = leadingZeros(this.getSeconds(),2);
     return year+ "/" + month + "/" + date + " " + hour + ":" + minutes + ":" + seconds; 
 }
 
-Date.prototype.stopWatchTime = function(time){
-    let timerDate = new Date(time);
-    let timerStr = timerDate.getMinutes() +":" + timerDate.getUTCSeconds() + ":" + timerDate.getMilliseconds();
 
-    return timerStr;
-}
 
 setInterval(
     ()=>{
@@ -52,30 +45,72 @@ setInterval(
 const startBtn = document.getElementById("start-btn");
 const stopBtn = document.getElementById("stop-btn");
 const recordBtn = document.getElementById("record-btn");
-const initBtn = document.getElementById("init-btn");
+const resetBtn = document.getElementById("reset-btn");
 
 const timeLabel = document.getElementById("stop-watch-timer");
+
+const recordDiv = document.getElementById("record-section");
 
 var stopWatchAction = null;
 var stopWatchRunning = false;
 
-var startTime = 0;
+Date.prototype.stopWatchTime = function(time){
+    let timerDate = new Date(time);
+    let timerStr =leadingZeros( timerDate.getMinutes(),2) +":" + leadingZeros(timerDate.getUTCSeconds(),2) + ":" + leadingZeros(timerDate.getMilliseconds(),3);
+
+    return timerStr;
+}
+
+var pastTime = 0;
+var curTime = 0;
+
+var curTimer = 0;
 
 startBtn.onclick = startTimer;
 stopBtn.onclick = stopTimer;
+recordBtn.onclick = recordTime;
+resetBtn.onclick = resetTimer;
 
 function startTimer(){
-    startTime = new Date().getTime();
-    if( !stopWatchRunning ){
-        timerAction = setInterval(
+    if( !stopWatchAction ){
+        curTime = new Date().getTime();
+
+        stopWatchAction = setInterval(
             () =>{
-                timeLabel.innerHTML = Date.prototype.stopWatchTime( new Date().getTime() - startTime);
+                pastTime = curTime;
+                curTime = new Date().getTime();
+                let spentTime = curTime - pastTime;
+                curTimer += spentTime;
+                timeLabel.innerHTML = Date.prototype.stopWatchTime( curTimer );
             }
             , 10
         )
     }
-        
 }
+
 function stopTimer(){
-    clearInterval( timerAction );
+        clearInterval( stopWatchAction );
+        stopWatchAction = null;
+}
+
+function resetTimer(){
+    curTimer = 0;
+    timeLabel.innerHTML = "00:00:000";
+    while ( recordDiv.hasChildNodes() ) { recordDiv.removeChild( recordDiv.firstChild ); }
+
+}
+
+function recordTime(){
+    let timeRecord = document.createElement("li");
+    timeRecord.innerHTML = Date.prototype.stopWatchTime(curTimer);
+    recordDiv.append( timeRecord );
+}
+
+
+function leadingZeros( time , length){
+    let str = time.toString();
+    while( str.length < length){
+        str = "0" + str;
+    }
+    return str;
 }
