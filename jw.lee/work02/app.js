15243c14f57f65f59fb6944a0f84f2b4874b14e2
이재원 <ijaewon@ijaewon-ui-MacBookPro.local>
2023-08-02 17:38:38 +0900

    Add Work02, Work03

--- jw.lee/work02/app.js
+++ jw.lee/work02/app.js
@@ -1,45 +1,82 @@
+import tempData from "./dummy.js";
 function app() {
+  // 초기 세팅
   const root = document.querySelector("#root");
+  const refreshBtn = document.createElement("button");
+  refreshBtn.innerHTML = "refresh";
+  refreshBtn.className = "refresh";
+  refreshBtn.onclick = refreshList;
+  root.appendChild(refreshBtn);
+
   const table = document.createElement("table");
   root.appendChild(table);
 
-  let tableHeader = document.createElement("tr");
-  table.appendChild(tableHeader);
+  // th 컴포넌트의 경우 재사용이 필요하여 function 분리
+  function renderTableHeader() {
+    const tableHeader = document.createElement("tr");
+    table.appendChild(tableHeader);
 
-  var thId = document.createElement("th");
-  thId.innerHTML = "id";
-  thId.className = "id";
-  tableHeader.appendChild(thId);
+    const thId = document.createElement("th");
+    thId.innerHTML = "id";
+    thId.className = "id";
+    tableHeader.appendChild(thId);
 
-  var thName = document.createElement("th");
-  thName.innerHTML = "employee_name";
-  thName.className = "employee_name";
-  tableHeader.appendChild(thName);
-  
-  var thSalary = document.createElement("th");
-  thSalary.innerHTML = "employee_salary";
-  thSalary.className = "employee_salary";
-  tableHeader.appendChild(thSalary);
-  
-  var thAge = document.createElement("th");
-  thAge.innerHTML = "employee_age";
-  thAge.className = "employee_age";
-  tableHeader.appendChild(thAge);
-  
-  var thImg = document.createElement("th");
-  thImg.innerHTML = "profile_image";
-  thImg.className = "profile_image";
-  tableHeader.appendChild(thImg);
-  
+    const thName = document.createElement("th");
+    thName.innerHTML = "employee_name";
+    thName.className = "employee_name";
+    tableHeader.appendChild(thName);
 
-  
+    const thSalary = document.createElement("th");
+    thSalary.innerHTML = "employee_salary";
+    thSalary.className = "employee_salary";
+    tableHeader.appendChild(thSalary);
 
+    const thAge = document.createElement("th");
+    thAge.innerHTML = "employee_age";
+    thAge.className = "employee_age";
+    tableHeader.appendChild(thAge);
 
-  
-  
-  
-  
-  
+    const thImg = document.createElement("th");
+    thImg.innerHTML = "profile_image";
+    thImg.className = "profile_image";
+    tableHeader.appendChild(thImg);
+  }
+  renderTableHeader();
+  // 초기 세팅 끝
+
+  // javascript fetch 사용 시 CORS 오류로 더미데이터로 임시 적용
+  function getDummyList() {
+    console.log("tempData : ", tempData);
+    const tableList = tempData.data;
+    tableList.forEach((el) => {
+      const newData = document.createElement("tr");
+      let id = document.createElement("td");
+      id.innerHTML = el.id;
+      let name = document.createElement("td");
+      name.innerHTML = el.employee_name;
+      let salary = document.createElement("td");
+      salary.innerHTML = el.employee_salary;
+      let age = document.createElement("td");
+      age.innerHTML = el.employee_age;
+      let profile = document.createElement("td");
+      profile.innerHTML = el.profile_image;
+      newData.appendChild(id);
+      newData.appendChild(name);
+      newData.appendChild(salary);
+      newData.appendChild(age);
+      newData.appendChild(profile);
+      table.appendChild(newData);
+    });
+  }
+
+  // 새로고침
+  function refreshList() {
+    table.innerHTML = "";
+    renderTableHeader();
+    getDummyList();
+  }
+
+  getDummyList();
 }
 
 app();
